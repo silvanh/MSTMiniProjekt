68e661ab39851de64df8c398558972e19e41f9c7
laquino <akku@outlook.com>
2015-11-18 13:11:23 +0100

    BL Service skeleton

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -1,14 +1,31 @@
-﻿namespace AutoReservation.BusinessLayer
+﻿using AutoReservation.Dal;
+
+namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
+        public void loadAutos()
+        {
+            using (var context = new AutoReservationEntities())
+            {
+            }
+        }
 
-        //private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
-        //{
-        //    var databaseValue = context.Entry(original).GetDatabaseValues();
-        //    context.Entry(original).CurrentValues.SetValues(databaseValue);
+        public void saveAuto(Auto auto)
+        {
+            using (var context = new AutoReservationEntities())
+            {
+                context.Auto.Add(auto);
+                context.SaveChanges();
+            }
+        }
 
-        //    throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
-        //}
+        private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
+        {
+            var databaseValue = context.Entry(original).GetDatabaseValues();
+            context.Entry(original).CurrentValues.SetValues(databaseValue);
+
+            throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
+        }
     }
 }
