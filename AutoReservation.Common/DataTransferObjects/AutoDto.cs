11695ad83726068d41ac319ed26b64fb918c8b36
laquino <akku@outlook.com>
2015-11-18 12:54:52 +0100

    implemented AutoDto-Properties

--- AutoReservation.Common/DataTransferObjects/AutoDto.cs
+++ AutoReservation.Common/DataTransferObjects/AutoDto.cs
@@ -1,54 +1,125 @@
 ﻿using AutoReservation.Common.Extensions;
 using AutoReservation.Common.DataTransferObjects.Core;
+using System.Text;
 
 namespace AutoReservation.Common.DataTransferObjects
 {
-    public class AutoDto //: DtoBase<AutoDto>
+    public class AutoDto : DtoBase<AutoDto>
     {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    this.OnPropertyChanged(p => p.Id);
+                }
+            }
+        }
+
+        private string marke;
+        public string Marke
+        {
+            get { return marke; }
+                
+            set
+            {
+                if (marke != value)
+                {
+                    marke = value;
+                    this.OnPropertyChanged(p => p.Marke);
+                }
+            }
+        }
+
+        private int tagestarif;
+        public int Tagestarif
+        {
+            get { return tagestarif; }
+            set
+            {
+                if (tagestarif != value)
+                {
+                    tagestarif = value;
+                    this.OnPropertyChanged(p => p.Tagestarif);
+                }
+            }
+        }
+
+        private int basistarif;
+        public int Basistarif
+        {
+            get { return basistarif; }
+            set
+            {
+                if (basistarif != value)
+                {
+                    basistarif = value;
+                    this.OnPropertyChanged(p => p.Basistarif);
+                }
+            }
+        }
+
+        private AutoKlasse autoKlasse;
+        public AutoKlasse AutoKlasse
+        {
+            get { return autoKlasse; }
+            set
+            {
+                if (autoKlasse != value)
+                {
+                    autoKlasse = value;
+                    this.OnPropertyChanged(p => p.AutoKlasse);
+                }
+            }
+        }
 
 
-        //public override string Validate()
-        //{
-        //    StringBuilder error = new StringBuilder();
-        //    if (string.IsNullOrEmpty(marke))
-        //    {
-        //        error.AppendLine("- Marke ist nicht gesetzt.");
-        //    }
-        //    if (tagestarif <= 0)
-        //    {
-        //        error.AppendLine("- Tagestarif muss grösser als 0 sein.");
-        //    }
-        //    if (AutoKlasse == AutoKlasse.Luxusklasse && basistarif <= 0)
-        //    {
-        //        error.AppendLine("- Basistarif eines Luxusautos muss grösser als 0 sein.");
-        //    }
+        public override string Validate()
+        {
+            StringBuilder error = new StringBuilder();
+            if (string.IsNullOrEmpty(marke))
+            {
+                error.AppendLine("- Marke ist nicht gesetzt.");
+            }
+            if (tagestarif <= 0)
+            {
+                error.AppendLine("- Tagestarif muss grösser als 0 sein.");
+            }
+            if (AutoKlasse == AutoKlasse.Luxusklasse && basistarif <= 0)
+            {
+                error.AppendLine("- Basistarif eines Luxusautos muss grösser als 0 sein.");
+            }
 
-        //    if (error.Length == 0) { return null; }
+            if (error.Length == 0) { return null; }
 
-        //    return error.ToString();
-        //}
+            return error.ToString();
+        }
 
-        //public override AutoDto Clone()
-        //{
-        //    return new AutoDto
-        //    {
-        //        Id = Id,
-        //        Marke = Marke,
-        //        Tagestarif = Tagestarif,
-        //        AutoKlasse = AutoKlasse,
-        //        Basistarif = Basistarif
-        //    };
-        //}
+        public override AutoDto Clone()
+        {
+            return new AutoDto
+            {
+                Id = Id,
+                Marke = Marke,
+                Tagestarif = Tagestarif,
+                AutoKlasse = AutoKlasse,
+                Basistarif = Basistarif
+            };
+        }
 
-        //public override string ToString()
-        //{
-        //    return string.Format(
-        //        "{0}; {1}; {2}; {3}; {4}",
-        //        Id,
-        //        Marke,
-        //        Tagestarif,
-        //        Basistarif,
-        //        AutoKlasse);
-        //}
+        public override string ToString()
+        {
+            return string.Format(
+                "{0}; {1}; {2}; {3}; {4}",
+                Id,
+                Marke,
+                Tagestarif,
+                Basistarif,
+                AutoKlasse);
+        }
     }
 }
