f3fdc7696dfc9f5fade6972c297aff45da56f06b
Valentin Meier <meier.valentin@gmail.com>
2015-11-18 14:39:00 +0100

    Updated Datamodel according to chapte 3.1 in PDF

--- AutoReservation.Dal/StandardAuto.cs
+++ AutoReservation.Dal/StandardAuto.cs
@@ -15,24 +15,8 @@ namespace AutoReservation.Dal
 using System;
     using System.Collections.Generic;
     
-public partial class Reservation
+public partial class StandardAuto : Auto
 {
-
-    public int ReservationNr { get; set; }
-
-    public int AutoId { get; set; }
-
-    public int KundeId { get; set; }
-
-    public System.DateTime Von { get; set; }
-
-    public System.DateTime Bis { get; set; }
-
-
-
-    public virtual Auto Autos { get; set; }
-
-    public virtual Kunde Kunden { get; set; }
 
 }
 
